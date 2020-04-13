deec3e35f89de180f0fef26192c29fae8374c721
Tammy Lam <lamtammy1515@gmail.com>
2020-04-13 02:22:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,11 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello World!"
+  end 
+  
   get '/reverse' do
     erb :reverse
   end
 
   post '/reverse' do
+   original_string = params["string"]
+   @reversed_string = original_string.reverse
+    
     erb :reversed
   end
 
